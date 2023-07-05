2df63d27f04af7df1b2775c63c8c2bf6fccb3a15
mateus <m4teusmatos@gmail.com>
2023-07-05 01:40:06 -0300

    imoporting components in page main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
+import { About } from '../components/About'
+import { Differences } from '../components/Differences'
+import { Differentials } from '../components/Differentials'
 import { Header } from '../components/Header'
 import { HeroSection } from '../components/HeroSection'
+import { Steps } from '../components/Steps'
 
 export default function Home() {
   return (
     <main>
       <Header />
       <HeroSection />
+      <Differentials />
+      <About />
+      <Steps />
+      <Differences />
     </main>
   )
 }
